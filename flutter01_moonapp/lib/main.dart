34a0cd98e665e42669d80dc52f8d76cd7b321727
BANDARAKDT <deshanibandarawas@gmail.com>
2024-02-27 22:46:32 +0530

    moonApp

--- flutter01_moonapp/lib/main.dart
+++ flutter01_moonapp/lib/main.dart
@@ -11,16 +11,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Space App",
       home: Scaffold(
         backgroundColor: Colors.black,
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 22, 33, 97),
           shadowColor: const Color.fromARGB(255, 245, 245, 10),
-          title: Text("BLACK HOLE",style: TextStyle(fontWeight:FontWeight.bold,color: Colors.yellowAccent),),
+          title: Text("BLACK HOLE",style: TextStyle(fontWeight:FontWeight.bold,color: Colors.yellowAccent),
         ),
-      ),
-    );
+        actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.menu),
+            )
+          ],),
 
+          body:Padding(
+            padding:const EdgeInsets.all(16.0),
+         
+            child: Column(
+              children: [
+                const Text("Space Details",
+                style: TextStyle(color:Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+                )
+                ],
+                ),
+          )
+    )
+    );
   }
   }
